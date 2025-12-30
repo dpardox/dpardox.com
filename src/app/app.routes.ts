2143bebbe68e47c34ff8059237dc207a46d6a204
Donovan Pardo <dpardox@gmail.com>
2025-12-29 19:12:14 -0500

    add routing configuration and implement change detection strategy in App component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
