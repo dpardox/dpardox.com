aaa5a2f680419f36bcfd2b77a7808bbdfc0a9af4
Donovan Pardo <dpardox@gmail.com>
2025-12-30 13:18:45 -0500

    refactor routing to use dynamic component loading for HomePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
+  {
+    path: '',
+    loadComponent: () => import('@features/home/home'),
+  },
   {
     path: '**',
     redirectTo: '',
