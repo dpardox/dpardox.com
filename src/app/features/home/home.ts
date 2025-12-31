19331579d40b9b63c831437ae1c4bd9d218faf1d
Donovan Pardo <dpardox@gmail.com>
2025-12-30 21:04:02 -0500

    feat: integrate footer component into home page and update logo component for dynamic mode

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '@shared/components/header/header';
 import { Banner } from './components/banner/banner';
 import { Networks } from 'src/app/pages/home/components/networks/networks';
+import { Footer } from '@shared/components/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Banner, Networks],
+  imports: [Header, Banner, Networks, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
--- src/app/shared/components/logo/logo.ts
+++ src/app/shared/components/logo/logo.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './logo.css',
 })
 export class Logo {
-
+  readonly mode = input<'light' | 'dark'>('light');
 }
