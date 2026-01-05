ddb8c62d34e985756a532b8f284b4db337651495
Donovan Pardo <dpardox@gmail.com>
2026-01-05 16:36:20 -0500

    style: update user references in networks component for consistency

--- src/app/pages/home/components/networks/networks.ts
+++ src/app/pages/home/components/networks/networks.ts
@@ -18,8 +18,8 @@ export class Networks {
 
   readonly networks = signal<Network[]>([
     { name: 'LinkedIn', user: 'linkedin.com/in/dpardox', link: 'https://www.linkedin.com/in/dpardox', image: 'linkedin.png' },
-    { name: 'GitHub', user: 'github.com/dpardox', link: 'https://github.com/dpardox', image: 'github.png' },
-    { name: 'Codewars', user: 'codewars.com/users/dpardox', link: 'https://www.codewars.com/users/dpardox/completed_solutions', image: 'codewars.svg' },
+    { name: 'GitHub', user: '@dpardox', link: 'https://github.com/dpardox', image: 'github.png' },
+    { name: 'Codewars', user: '@dpardox', link: 'https://www.codewars.com/users/dpardox/completed_solutions', image: 'codewars.svg' },
   ]);
 
 }
